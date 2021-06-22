6cff41ec3fdf0d4a22c1c456a7d08d8e43697c1e
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2021-06-22 13:48:34 +0900

    Update SimpleWrite.java

--- src/main/java/SimpleWrite.java
+++ src/main/java/SimpleWrite.java
@@ -1,2 +1,30 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SimpleWrite {
+
+    public static void main(String[] args) throws IOException {
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String now = LocalDateTime.now().format(format);
+        System.out.println(now);
+
+        String outDir = System.getenv("PDF_OUT_DIR");
+        File outFile = new File(outDir + "/out_" + now + ".pdf");
+
+        try (PDDocument document = new PDDocument();) {
+
+            PDPage page = new PDPage(PDRectangle.A4);
+            page.setRotation(90);
+            document.addPage(page);
+
+            document.save(outFile);
+        }
+    }
 }
